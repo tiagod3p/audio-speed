a5c68f541f838c263fbd2e4f898e8354daf6ea78
Tiago Vidal <tiagod3p@gmail.com>
2021-04-21 18:24:16 -0300

    feat: add button to add and subtract speed

--- content.js
+++ content.js
@@ -4,9 +4,25 @@ const headerInterval = setInterval(() => {
   if (header) {
     clearInterval(headerInterval);
 
+    const INITIAL_SPEED = 1;
+
     const speedContainer = document.createElement("div");
     speedContainer.classList.add("speed-container");
     header.appendChild(speedContainer);
 
+    const subtractButton = document.createElement("button");
+    subtractButton.innerHTML = "-";
+    subtractButton.classList.add("speed-button");
+    speedContainer.appendChild(subtractButton);
+
+    const speedDisplay = document.createElement("span");
+    speedDisplay.innerHTML = INITIAL_SPEED.toFixed(2);
+    speedDisplay.classList.add("speed-display");
+    speedContainer.appendChild(speedDisplay);
+
+    const addButton = document.createElement("button");
+    addButton.innerHTML = "+";
+    addButton.classList.add("speed-button");
+    speedContainer.appendChild(addButton);
   }
 }, 1000);
